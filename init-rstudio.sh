a97616787658dd5375075762646befffa8b63333
Florent Bédécarrats <f.bedecarrats@gmail.com>
2021-11-24 22:35:16 +0100

    add data and documentation

--- init-rstudio.sh
+++ init-rstudio.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 
+# Create variables
 WORK_DIR=/home/rstudio/analyse_mobilite_rp2018
 REPO_URL=https://github.com/fBedecarrats/analyse_mobilite_rp2018
 DATA_DIR=${WORK_DIR}/data
+DOC_DIR=${WORK_DIR}/documentation
 
+# Git
 git clone $REPO_URL $WORK_DIR
 chown -R rstudio:users $WORK_DIR
 
+# Folders to store data and documentation
 mkdir $DATA_DIR
+mkdir $DOC_DIR
 
-mc cp s3/fbedecarrats/diffusion/FD_MOBPRO_2018.csv $DATA_DIR
+# copy files
+mc cp s3/fbedecarrats/diffusion/{FD_MOBPRO_2018.csv,commune2021.csv,Intercommunalite-Metropole_au_01-01-2021.xlsx,Varmod_MOBPRO_2018.csv} $DATA_DIR
+mc cp s3/fbedecarrats/diffusion/contenu_RP2018_mobpro.pdf $DATA_DIR
